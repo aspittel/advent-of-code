a34a0a0ae87ee50b7aff79afe6323deb3a004246
Ali Spittel <ali07cat07@gmail.com>
2018-12-01 10:23:42 -0500

    add part 2

---
+++ dec-1/script-pt-2.py
@@ -0,0 +1,14 @@
+data = [i for i in open('./input.txt', 'r')]
+
+def get_first_duplicate_total(data):
+    total = 0
+    prev_totals = set([0])
+    while True:
+        for line in data:
+            total += int(line)
+            if total in prev_totals:
+                return total
+            prev_totals.add(total)
+    return total
+
+print(get_first_duplicate_total(data))
