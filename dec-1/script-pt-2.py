c320ea2f85c5d4dd8883225e32bc3531f8cb639e
Ali Spittel <ali07cat07@gmail.com>
2018-12-01 10:51:20 -0500

    rename variable in loop

--- dec-1/script-pt-2.py
+++ dec-1/script-pt-2.py
@@ -4,8 +4,8 @@ def get_first_duplicate_total(data):
     total = 0
     prev_totals = set([0])
     while True:
-        for line in data:
-            total += line
+        for i in data:
+            total += i
             if total in prev_totals:
                 return total
             prev_totals.add(total)
