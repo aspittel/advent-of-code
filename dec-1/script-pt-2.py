344de8ddcf2962fd511ff4e369a9090c90219882
Ali Spittel <ali07cat07@gmail.com>
2018-12-01 10:40:36 -0500

    move int conversion

--- dec-1/script-pt-2.py
+++ dec-1/script-pt-2.py
@@ -1,11 +1,11 @@
-data = [i for i in open('./input.txt', 'r')]
+data = [int(i) for i in open('./input.txt', 'r')]
 
 def get_first_duplicate_total(data):
     total = 0
     prev_totals = set([0])
     while True:
         for line in data:
-            total += int(line)
+            total += line
             if total in prev_totals:
                 return total
             prev_totals.add(total)
