f66a2db02d09bf776eb38ebdf70c9a7c5d46d812
Ali Spittel <ali07cat07@gmail.com>
2018-12-03 09:20:03 -0500

    refactor

--- dec-3/script.py
+++ dec-3/script.py
@@ -1,38 +1,46 @@
+import re
+
 with open('input.txt', 'r') as f:
     data = []
     for line in f:
-        line = line.strip().split('@ ')
-        _id = line[0]
-        line = line[1].split(': ')
-        coordinates = [int(i) for i in line[0].split(',')]
-        dimensions = [int(i) for i in line[1].split('x')]
-        data.append({'id': _id, 'coordinates': coordinates, 'dimensions': dimensions})
+        nums = [int(n) for n in re.findall(r'\d+', line)]
+        data.append({'id': nums[0], 'coordinates': [nums[1], nums[2]], 'dimensions': [nums[3], nums[4]]})
 
-overlaps = set()
-filled = set()
 
-def get_overlaps(coordinates, dimensions):
+def get_coordinates(coordinates, dimensions):
     for x in range(dimensions[0]):
         for y in range(dimensions[1]):
-            coord = str(x + coordinates[0]) + "," + str(y + coordinates[1])
+            yield str(x + coordinates[0]) + "," + str(y + coordinates[1])
+
+
+def get_overlaps(data):
+    overlaps = set()
+    filled = set()
+    for line in data:
+        for coord in get_coordinates(line['coordinates'], line['dimensions']):
             if coord in filled:
                 overlaps.add(coord)
             else:
                 filled.add(coord)
+    return overlaps
 
-def no_overlaps(coordinates, dimensions):
-    for x in range(dimensions[0]):
-        for y in range(dimensions[1]):
-            coord = str(x + coordinates[0]) + "," + str(y + coordinates[1])
-            if coord in overlaps: return False
+
+def no_overlaps(coordinates, dimensions, overlaps):
+    for coord in get_coordinates(coordinates, dimensions):
+        if coord in overlaps: 
+            return False
     return True
 
+ 
+def find_no_overlaps(data, overlaps):
+    for line in data:
+        if no_overlaps(line['coordinates'], line['dimensions'], overlaps):
+            return line['id']
+
+
+overlaps = get_overlaps(data)
 # Q1
-for line in data:
-    get_overlaps(line['coordinates'], line['dimensions'])
 print(len(overlaps))
 
 # Q2
-for line in data:
-    if no_overlaps(line['coordinates'], line['dimensions']):
-        print(line['id'])
+print(find_no_overlaps(data, overlaps))
