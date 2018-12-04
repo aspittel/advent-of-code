34078f254a83b586d39b3fa5b554107a70407b6b
Ali Spittel <ali07cat07@gmail.com>
2018-12-04 09:24:19 -0500

    unrefactored solution

---
+++ dec-4/script-pt-2.py
@@ -0,0 +1,33 @@
+import re
+from datetime import datetime, timedelta
+from collections import Counter
+
+with open('input.txt', 'r') as f:
+    data = []
+    for line in f:
+       date = line[line.find("[")+1:line.find("]")]
+       action = line.split("] ")[1].strip()
+       data.append((date, action,))
+    data = sorted(data, key=lambda i: i[0])
+
+guards = {}
+for time, action in data:
+    time = int(time[-2:])
+    if "Guard" in action:
+        _id = re.findall(r'\d+', action)[0]
+        if not _id in guards:
+            guards[_id] = []
+    elif action == "falls asleep":
+        start = time
+    else:
+        guards[_id] += list(range(start, time))
+
+max_count = 0
+solution = 0
+for guard, minutes in guards.items():
+    if minutes:
+        most_frequent = Counter(minutes).most_common(1)[0]
+        if most_frequent[1] > max_count:
+            max_count = most_frequent[1]
+            solution = int(guard) * most_frequent[0]
+print(solution)
---
+++ dec-4/script.py
@@ -0,0 +1,28 @@
+import re
+from datetime import datetime, timedelta
+from collections import Counter
+
+with open('input.txt', 'r') as f:
+    data = []
+    for line in f:
+       date = line[line.find("[")+1:line.find("]")]
+       action = line.split("] ")[1].strip()
+       data.append((date, action,))
+    data = sorted(data, key=lambda i: i[0])
+
+guards = {}
+for time, action in data:
+    time = int(time[-2:])
+    if "Guard" in action:
+        _id = re.findall(r'\d+', action)[0]
+        if not _id in guards:
+            guards[_id] = {'length': 0, 'minutes': []}
+    elif action == "falls asleep":
+        start = time
+    else:
+        guards[_id]['length'] += time - start
+        guards[_id]['minutes'] += list(range(start, time))
+        
+sleep_longest = max(guards.items(),key=lambda guard: guard[1]['length'])
+minutes = sleep_longest[1]['minutes']
+print(Counter(minutes).most_common(1)[0][0] * int(sleep_longest[0]))
