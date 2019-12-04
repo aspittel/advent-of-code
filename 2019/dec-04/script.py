e7e3335e60ccf36a42c160696bc535e84aef49f2
Ali Spittel <ali07cat07@gmail.com>
2019-12-04 07:12:17 -0600

    day 4

---
+++ 2019/dec-04/script.py
@@ -0,0 +1,24 @@
+increasing_numbers = set()
+
+def check_number(n):
+    double_digit = False
+    n_repeating = 0
+    n = str(n)
+    for n1, n2 in zip(n, n[1:]):
+        if n1 > n2:
+            return False
+        elif n1 == n2:
+            n_repeating += 1
+        else:
+            if n_repeating == 1:
+                double_digit = True
+            n_repeating = 0
+    if n_repeating == 1:
+        double_digit = True
+    return double_digit 
+
+good_passwords_count = 0
+for n in range(134792, 675810 + 1):
+    if check_number(n):
+        good_passwords_count += 1
+print(good_passwords_count)
