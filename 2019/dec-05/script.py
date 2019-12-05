a1fc8bc85f648eb027694808098b5ee430f358fa
Ali Spittel <ali07cat07@gmail.com>
2019-12-05 11:06:25 -0600

    vals => params

--- 2019/dec-05/script.py
+++ 2019/dec-05/script.py
@@ -2,38 +2,38 @@ def get_modes(modes):
     return [int(mode) for mode in [modes[2], modes[1], modes[0], modes[3:]]]
 
 
-def get_val(mode, increment, idx, data):
+def get_param(mode, increment, idx, data):
     if mode == 0:
         return data[data[idx + increment]]
     return data[idx + increment]
 
-def get_vals(mode1, mode2, idx, data):
-    return get_val(mode1, 1, idx, data), get_val(mode2, 2, idx, data)
+def get_params(mode1, mode2, idx, data):
+    return get_param(mode1, 1, idx, data), get_param(mode2, 2, idx, data)
 
 
 def add(mode1, mode2, idx, data):
-    val1, val2 = get_vals(mode1, mode2, idx, data)
-    data[data[idx + 3]] = val1 + val2
+    param1, param2 = get_params(mode1, mode2, idx, data)
+    data[data[idx + 3]] = param1 + param2
 
 def multiply(mode1, mode2, idx, data):
-    val1, val2 = get_vals(mode1, mode2, idx, data)
-    data[data[idx + 3]] = val1 * val2
+    param1, param2 = get_params(mode1, mode2, idx, data)
+    data[data[idx + 3]] = param1 * param2
 
 def less_than(mode1, mode2, idx, data):
-    val1, val2 = get_vals(mode1, mode2, idx, data)
-    data[data[idx + 3]] = 1 if val1 < val2 else 0
+    param1, param2 = get_params(mode1, mode2, idx, data)
+    data[data[idx + 3]] = 1 if param1 < param2 else 0
 
 def equals(mode1, mode2, idx, data):
-    val1, val2 = get_vals(mode1, mode2, idx, data)
-    data[data[idx + 3]] = 1 if val1 == val2 else 0
+    param1, param2 = get_params(mode1, mode2, idx, data)
+    data[data[idx + 3]] = 1 if param1 == param2 else 0
 
 def jump_if_true(mode1, mode2, idx, data):
-    val1, val2 = get_vals(mode1, mode2, idx, data)
-    return val2 if val1 != 0 else idx + 3
+    param1, param2 = get_params(mode1, mode2, idx, data)
+    return param2 if param1 != 0 else idx + 3
 
 def jump_if_false(mode1, mode2, idx, data):
-    val1, val2 = get_vals(mode1, mode2, idx, data)
-    return val2 if val1 == 0 else idx + 3
+    param1, param2 = get_params(mode1, mode2, idx, data)
+    return param2 if param1 == 0 else idx + 3
 
 
 def calculate(data, input_val):
@@ -68,7 +68,7 @@ def calculate(data, input_val):
 
 with open("input.txt") as _file:
     for line in _file:
-        input_values = [int(num) for num in line.split(",")]
-        print(f"Part 1: {calculate(input_values[:], 1)}")
-        print(f"Part 2: {calculate(input_values[:], 5)}")
+        input_vals = [int(num) for num in line.split(",")]
+        print(f"Part 1: {calculate(input_vals[:], 1)}")
+        print(f"Part 2: {calculate(input_vals[:], 5)}")
 
