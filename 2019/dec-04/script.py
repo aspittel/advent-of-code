1dc119f5e8f327d477e3f3e8d22f65ebd168bbb5
Ali Spittel <ali07cat07@gmail.com>
2019-12-04 22:18:33 -0600

    refactor

--- 2019/dec-04/script.py
+++ 2019/dec-04/script.py
@@ -1,39 +1,34 @@
-def check_number_double_repeats(n):
-    double_digit = False
-    n_repeating = 0
-    n = str(n)
+def check_ascending(n):
+    return "".join(sorted(n)) == n
+
+
+def check_repeat(n):
+    for digit1, digit2 in zip(n, n[1:]):
+        if digit1 == digit2:
+            return True
+
+
+def check_two_consecutive_digits(n):
+    repeat_count = 0
     for n1, n2 in zip(n, n[1:]):
-        if n1 > n2:
-            return False
-        elif n1 == n2:
-            n_repeating += 1
+        if n1 == n2:
+            repeat_count += 1
         else:
-            if n_repeating == 1:
-                double_digit = True
-            n_repeating = 0
-    # catch a repeat at the end
-    if n_repeating == 1:
-        double_digit = True
-    return double_digit 
-
-
-def check_number(n):
-    repeat = False
-    n = str(n)
-    for n1, n2 in zip(n, n[1:]):
-        if n1 > n2:
-            return False
-        elif n1 == n2:
-            repeat = True
-    return repeat 
+            if repeat_count == 1:
+                return True
+            repeat_count = 0
+    return repeat_count == 1
 
 p1_count = 0
 p2_count = 0
-for n in range(134792, 675810 + 1):
-    if check_number(n):
-        p1_count += 1
-    if check_number_double_repeats(n):
-        p2_count += 1
-        
-print(p1_count)
-print(p2_count)
+
+for n in range(134792, 675811):
+    n = str(n)
+    if check_ascending(n):
+        if check_repeat(n):
+            p1_count += 1
+        if check_two_consecutive_digits(n):
+            p2_count += 1
+
+print(f"Part 1: {p1_count}")
+print(f"Part 2: {p2_count}")
