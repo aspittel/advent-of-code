132a1cf2a4ac93330e3c5bad5099d6e5e327fcc9
Ali Spittel <ali07cat07@gmail.com>
2019-12-06 21:31:11 -0600

    clean up day 5

--- 2019/dec-05/script.py
+++ 2019/dec-05/script.py
@@ -14,18 +14,22 @@ def get_params(mode1, mode2, idx, data):
 def add(mode1, mode2, idx, data):
     param1, param2 = get_params(mode1, mode2, idx, data)
     data[data[idx + 3]] = param1 + param2
+    return idx + 4
 
 def multiply(mode1, mode2, idx, data):
     param1, param2 = get_params(mode1, mode2, idx, data)
     data[data[idx + 3]] = param1 * param2
+    return idx + 4
 
 def less_than(mode1, mode2, idx, data):
     param1, param2 = get_params(mode1, mode2, idx, data)
     data[data[idx + 3]] = 1 if param1 < param2 else 0
+    return idx + 4
 
 def equals(mode1, mode2, idx, data):
     param1, param2 = get_params(mode1, mode2, idx, data)
     data[data[idx + 3]] = 1 if param1 == param2 else 0
+    return idx + 4
 
 def jump_if_true(mode1, mode2, idx, data):
     param1, param2 = get_params(mode1, mode2, idx, data)
@@ -41,28 +45,22 @@ def calculate(data, input_val):
     diagnostic_code = None
     while data[idx] != 99:
         mode1, mode2, mode3, opcode = get_modes(f"{data[idx]:05}")
-        if opcode == 1:
-            add(mode1, mode2, idx, data)
-            idx += 4
-        elif opcode == 2:
-            multiply(mode1, mode2, idx, data)
-            idx += 4
+        OPCODES = {
+            1: add,
+            2: multiply,
+            5: jump_if_true,
+            6: jump_if_false,
+            7: less_than,
+            8: equals
+        }
+        if opcode in OPCODES:
+            idx = OPCODES[opcode](mode1, mode2, idx, data)
         elif opcode == 3:
             data[data[idx+1]] = input_val
             idx += 2
         elif opcode == 4:
             diagnostic_code = data[data[idx + 1]]
             idx += 2
-        elif opcode == 5:
-            idx = jump_if_true(mode1, mode2, idx, data)
-        elif opcode == 6:
-            idx = jump_if_false(mode1, mode2, idx, data)
-        elif opcode == 7:
-            less_than(mode1, mode2, idx, data)
-            idx += 4
-        elif opcode == 8:
-            equals(mode1, mode2, idx, data)
-            idx += 4
     return diagnostic_code
 
 
