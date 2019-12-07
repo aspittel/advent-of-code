9b95d5558b0b1ed5e478df571141431956d4db5b
Ali Spittel <ali07cat07@gmail.com>
2019-12-06 18:15:55 -0600

    standardize code

--- 2019/dec-06/script.py
+++ 2019/dec-06/script.py
@@ -20,7 +20,7 @@ def get_total_orbits(orbits):
     return sum(len(get_orbit(obj, orbits)) for obj in objects)
 
 
-def path_between(start, end, orbits):
+def get_path_between(start, end, orbits):
     start_path, end_path = get_orbit(start, orbits), get_orbit(end, orbits)
     # Get the first point that is in both paths
     overlapping_point = [i for i in start_path if i in end_path][0]
@@ -31,4 +31,4 @@ with open("input.txt") as _file:
     orbits, objects = clean_data(_file)
 
 print(f"Part 1: {get_total_orbits(orbits)}")
-print(f'Part 2: {path_between("YOU", "SAN", orbits)}')
+print(f'Part 2: {get_path_between("YOU", "SAN", orbits)}')
