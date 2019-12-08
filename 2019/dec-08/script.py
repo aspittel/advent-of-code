7a529f1e9e691e543ce9116160bad06ce551ecbe
Ali Spittel <ali07cat07@gmail.com>
2019-12-08 01:43:17 -0600

    clean code

--- 2019/dec-08/script.py
+++ 2019/dec-08/script.py
@@ -1,8 +1,15 @@
-def get_layer(x, y, data):
+def get_layer(data, y=1, x=1):
     start = 0
     while start < len(data):
         yield data[start:start + (x * y)]
         start += x * y
+
+def get_pixel(layers, idx):
+    for layer in layers:
+        if layer[idx] == "1":
+            return "1"
+        elif layer[idx] == "0":
+            return " "
 
 
 LAYER_HEIGHT = 6
@@ -10,26 +17,17 @@ LAYER_WIDTH = 25
 
 with open('input.txt') as _file:
     data = _file.read()
-    layers = [layer for layer in get_layer(LAYER_HEIGHT, LAYER_WIDTH, data)]
+    layers = [layer for layer in get_layer(data, LAYER_HEIGHT, LAYER_WIDTH)]
     least_zeroes = sorted(layers, key=lambda x: x.count("0"))[0]
     print(f'Part 1: {least_zeroes.count("1") * least_zeroes.count("2")}')
 
     stacked_pixels = ""
     for coordinate in range(LAYER_HEIGHT * LAYER_WIDTH):
-        done = False
-        starting_idx = 0
-        while not done:
-            if layers[starting_idx][coordinate] == "2":
-                starting_idx += 1
-            else:
-                done = True
-                stacked_pixels += " " if layers[starting_idx][coordinate] == "0" else "1"
+        stacked_pixels += get_pixel(layers, coordinate)
 
     start = 0
     print("Part 2: ")
-    print(''.join(layer) for layer in get_layer(LAYER_WIDTH, 1, stacked_pixels))
-    # while start < len(stacked_pixels):
-    #     print(stacked_pixels[start:start + LAYER_WIDTH])
-    #     start += 25
+    for layer in get_layer(stacked_pixels, LAYER_WIDTH):
+        print(''.join(layer))
 
 
