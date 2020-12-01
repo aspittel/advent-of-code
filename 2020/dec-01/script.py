84c5ce33a57a8eee9c4f05e5d62c3834a73fd737
Ali Spittel <ali07cat07@gmail.com>
2020-12-01 08:41:42 -0600

    clean up script

--- 2020/dec-01/script.py
+++ 2020/dec-01/script.py
@@ -1,8 +1,19 @@
-with open("input.txt") as _file:
-    nums = [int(line) for line in _file]
-    set_nums = set(nums)
+def part_one(nums, set_nums):
+    for num in nums:
+        if 2020 - num in set_nums:
+            return num * (2020 - num)
+
+
+def part_two(nums, set_nums):
     for num in nums:
         for num2 in nums:
             if (2020 - num - num2) in set_nums:
-                print(num * (2020-num-num2) * num2)
-                break
+                return num * (2020 - num - num2) * num2
+
+
+with open("input.txt") as _file:
+    nums = [int(line) for line in _file]
+    set_nums = set(nums)
+
+    print("Part 1", part_one(nums, set_nums))
+    print("Part 2", part_two(nums, set_nums))
