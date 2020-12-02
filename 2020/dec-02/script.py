b0349bd898f0e0423b38be29985b98cc153b0d7f
Ali Spittel <ali07cat07@gmail.com>
2020-12-01 23:14:26 -0600

    add part 1

--- 2020/dec-02/script.py
+++ 2020/dec-02/script.py
@@ -1,16 +1,24 @@
 with open('input.txt') as file:
     letters = [num.split(": ") for num in file]
     tally = 0
+    tally2 = 0
     for policy, password in letters:
         length, letter = policy.split(" ")
         start, stop = length.split("-")
-        start, stop = int(start) - 1, int(stop) - 1
+        start, stop = int(start), int(stop)
+
+        # part 1
+        if password.count(letter) >= start and password.count(letter) <= stop:
+            tally += 1
+
+        # part 2
+        start, stop = start - 1, stop - 1
         try:
             if (password[start] == letter or password[stop] == letter) and (password[start] != password[stop]):
-                tally += 1
-                print(password)
+                tally2 += 1
         except:
             pass
         
 
     print(tally)
+    print(tally2)
