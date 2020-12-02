11a556d4b8aa88c9f29097155cfbae989d1e7c3f
Ali Spittel <ali07cat07@gmail.com>
2020-12-02 10:29:28 -0600

    refactor

--- 2020/dec-02/script.py
+++ 2020/dec-02/script.py
@@ -1,24 +1,25 @@
+def check_count_in_range(password, letter, start, stop):
+    return password.count(letter) >= start and password.count(letter) <= stop
+
+
+def check_indexes(password, letter, start, stop):
+    return (password[start] == letter or password[stop] == letter) and (password[start] != password[stop])
+
 with open('input.txt') as file:
     letters = [num.split(": ") for num in file]
-    tally = 0
-    tally2 = 0
+    part1_tally = 0
+    part2_tally = 0
     for policy, password in letters:
         length, letter = policy.split(" ")
         start, stop = length.split("-")
         start, stop = int(start), int(stop)
 
         # part 1
-        if password.count(letter) >= start and password.count(letter) <= stop:
-            tally += 1
+        if check_count_in_range(password, letter, start, stop): part1_tally += 1
 
         # part 2
-        start, stop = start - 1, stop - 1
-        try:
-            if (password[start] == letter or password[stop] == letter) and (password[start] != password[stop]):
-                tally2 += 1
-        except:
-            pass
+        if check_indexes(password, letter, start - 1, stop - 1): part2_tally += 1
         
 
-    print(tally)
-    print(tally2)
+    print("Part 1", part1_tally)
+    print("Part 2", part2_tally)
