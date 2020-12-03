d34fbd0d315b14ce3671ecea1d9fbc24e7b261b6
Ali Spittel <ali07cat07@gmail.com>
2020-12-03 14:14:15 -0600

    day 3

---
+++ 2020/dec-03/script.py
@@ -0,0 +1,33 @@
+import math
+
+right_list = [1, 3, 5, 7, 1]
+down_list = [1, 1, 1, 1, 2]
+
+def get_trees_for_slope(right, down, lines):
+    x = right
+    y = down
+    trees = 0
+    while y < len(lines):
+        if x > len(lines[y]):
+            x = x % len(lines[y][0])
+        if lines[y][x] == '#':
+            trees += 1
+        x += right
+        y += down
+    return trees
+
+
+with open('input.txt') as inp:
+    lines = [line for line in inp]
+    height = len(lines)
+    width = len(lines[0])
+    cols_needed = ((height * 10) // width) + 2
+    lines = [list(line.rstrip() * 102) for line in lines]
+
+    print("Part 1", get_trees_for_slope(3, 1, lines))
+
+    all_trees = 1
+    for (right, down) in zip(right_list, down_list):
+        all_trees *= get_trees_for_slope(right, down, lines)
+    
+    print("Part 2", all_trees)
