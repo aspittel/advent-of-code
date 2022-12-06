9f122a884468211bc3631517e5bcf4592e9c756b
Ali Spittel <ali07cat07@gmail.com>
2022-12-06 08:45:10 -0700

    2022

--- 2022/06/script.py
+++ 2022/06/script.py
@@ -6,7 +6,6 @@ MARKER_LENGTH = 14
 for char in range(MARKER_LENGTH, len(word)):
     substr = word[char - MARKER_LENGTH:char]
     if len(set(substr)) == MARKER_LENGTH:
-        print(substr)
         print(char)
         break
 
